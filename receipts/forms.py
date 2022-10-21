01d3d7bbafe1d5b7543200b6da84352ba61d60f8
Jamal <jamal.degeratto@gmail.com>
2022-10-20 21:23:30 -0500

    Feature 14 complete

--- receipts/forms.py
+++ receipts/forms.py
@@ -1,21 +1,27 @@
 from django.forms import ModelForm
-from receipts.models import Receipt, ExpenseCategory
+from receipts.models import Receipt, ExpenseCategory, Account
 
 
 class ReceiptForm(ModelForm):
     class Meta:
         model = Receipt
-        fields = [
+        fields = (
             "vendor",
             "total",
             "tax",
             "date",
             "category",
             "account",
-        ]
+        )
 
 
 class CategoryForm(ModelForm):
     class Meta:
         model = ExpenseCategory
         fields = ("name",)
+
+
+class AccountForm(ModelForm):
+    class Meta:
+        model = Account
+        fields = ("name", "number")
